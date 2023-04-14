5b6093e73e9fa380bb16ac80c90b6b4d0e5368cf
kirino.minato@gmail.com <kirino.minato@gmail.com>
2023-04-15 07:47:58 +0900

    Minor wording corrections

--- src/App.vue
+++ src/App.vue
@@ -29,11 +29,11 @@ global.on("event", async (ev) => {
       ? a.id === b.id
         ? 0
         : a.id < b.id
-        ? 1
-        : -1
+          ? 1
+          : -1
       : a.created_at < b.created_at
-      ? 1
-      : -1;
+        ? 1
+        : -1;
   });
   events.value = events.value.filter((event, index, array) => {
     return index === 0 || event.id !== array[index - 1].id;
@@ -79,45 +79,29 @@ setInterval(collectPubkeys, 60000);
           <h2 class="p-index-intro__head">はじめに</h2>
           <p class="p-index-intro__text">Nostrを始めてみたくなった方は</p>
           <p class="p-index-intro__text">
-            <a
-              href="https://scrapbox.io/nostr/%E3%81%AF%E3%81%98%E3%82%81%E3%81%A6%E3%81%AENostr%E3%80%90%E3%81%AF%E3%81%98%E3%82%81%E3%81%A6%E3%81%AE%E6%96%B9%E3%81%AF%E3%81%93%E3%81%A1%E3%82%89%E3%80%91"
-              rel="noopener"
-              target="_blank"
-              class="p-index-intro__btn"
-              >はじめてのNostr【はじめての方はこちら】</a
-            >
+            <a href="https://scrapbox.io/nostr/%E3%81%AF%E3%81%98%E3%82%81%E3%81%A6%E3%81%AENostr%E3%80%90%E3%81%AF%E3%81%98%E3%82%81%E3%81%A6%E3%81%AE%E6%96%B9%E3%81%AF%E3%81%93%E3%81%A1%E3%82%89%E3%80%91"
+              rel="noopener" target="_blank" class="p-index-intro__btn">はじめてのNostr【はじめての方はこちら】</a>
           </p>
           <p class="p-index-intro__text">を参照ください。<br /></p>
           <p class="p-index-intro__text">
             また、日本向けリレーとしてこのページの裏側にある
-            <code>wss://relay-jp.nostr.wirednet.jp</code> をお使いください。
+            <code>wss://relay-jp.nostr.wirednet.jp</code> も是非お使いください。
           </p>
         </div>
       </div>
     </div>
     <div class="p-index-feeds">
-      <div
-        v-for="e in events"
-        v-bind:key="nostr.nip19.noteEncode(e.id)"
-        class="c-feed-item"
-      >
+      <div v-for="e in events" v-bind:key="nostr.nip19.noteEncode(e.id)" class="c-feed-item">
         <div class="c-feed-profile">
           <p class="c-feed-profile__avatar">
-            <img
-              class="profilePicture"
-              v-bind:src="
-                profiles.get(e.pubkey)?.picture ??
-                'https://placehold.jp/60x60.png'
-              "
-            />
+            <img class="profilePicture" v-bind:src="
+              profiles.get(e.pubkey)?.picture ??
+              'https://placehold.jp/60x60.png'
+            " />
           </p>
-          <a
-            target="_blank"
-            v-bind:href="
-              'https://nostx.shino3.net/' + nostr.nip19.npubEncode(e.pubkey)
-            "
-            class="c-feed-profile__detail"
-          >
+          <a target="_blank" v-bind:href="
+            'https://nostx.shino3.net/' + nostr.nip19.npubEncode(e.pubkey)
+          " class="c-feed-profile__detail">
             <span class="c-feed-profile__display-name">
               {{ profiles.get(e.pubkey)?.display_name ?? "noname" }}
             </span>
@@ -130,14 +114,10 @@ setInterval(collectPubkeys, 60000);
           {{ e.content.replace("\n", "\n") }}
         </p>
         <p class="c-feed-date">
-          <a
-            target="_blank"
-            v-bind:href="
-              'https://nostx.shino3.net/' + nostr.nip19.noteEncode(e.id)
-            "
-          >
-            {{ new Date(e.created_at * 1000).toLocaleString() }}</a
-          >
+          <a target="_blank" v-bind:href="
+            'https://nostx.shino3.net/' + nostr.nip19.noteEncode(e.id)
+          ">
+            {{ new Date(e.created_at * 1000).toLocaleString() }}</a>
         </p>
       </div>
     </div>
@@ -152,6 +132,7 @@ setInterval(collectPubkeys, 60000);
   display: flex;
   gap: 20px;
 }
+
 @media screen and (max-width: 880px) {
   .p-index-wrap {
     display: block;
@@ -163,14 +144,17 @@ setInterval(collectPubkeys, 60000);
   flex-shrink: 0;
   position: relative;
 }
+
 .p-index-heading__inner {
   position: sticky;
   top: 0;
 }
+
 @media screen and (max-width: 880px) {
   .p-index-heading {
     width: 100%;
   }
+
   .p-index-heading__inner {
     position: relative;
   }
@@ -179,6 +163,7 @@ setInterval(collectPubkeys, 60000);
 .p-index-feeds {
   padding: 2rem 0;
 }
+
 .p-index-title {
   font-family: "Anton", sans-serif;
   color: #ffffff;
@@ -188,9 +173,11 @@ setInterval(collectPubkeys, 60000);
   margin: 0;
   padding: 2rem 0;
 }
+
 .p-index-title__main {
   font-size: 6rem;
 }
+
 .p-index-title__sub {
   font-size: 1.5rem;
 }
@@ -201,9 +188,11 @@ setInterval(collectPubkeys, 60000);
     flex-wrap: wrap;
     gap: 0.4rem;
   }
+
   .p-index-title__main {
     font-size: 3rem;
   }
+
   .p-index-title__sub {
     font-size: 1.5rem;
     width: 100%;
@@ -213,10 +202,11 @@ setInterval(collectPubkeys, 60000);
 .p-index-intro {
   margin-top: 2rem;
 }
+
 @media screen and (max-width: 880px) {
   .p-index-intro {
-  margin-top: 0rem;
-}
+    margin-top: 0rem;
+  }
 }
 
 
@@ -228,12 +218,14 @@ setInterval(collectPubkeys, 60000);
   gap: 0.4rem;
   align-items: center;
 }
+
 .p-index-intro__head::after {
   content: "";
   width: 3em;
   height: 2px;
   background-color: #ffffff;
 }
+
 .p-index-intro__text {
   color: #ffffff;
   margin: 0;
@@ -244,6 +236,7 @@ setInterval(collectPubkeys, 60000);
 .p-index-intro__text code {
   display: inline-block;
 }
+
 .p-index-intro__btn {
   background-color: #fc5fa1;
   color: #ffffff;
@@ -252,6 +245,7 @@ setInterval(collectPubkeys, 60000);
   border-radius: 2rem;
   transition: all 0.4s;
 }
+
 .p-index-intro__btn:hover {
   background-color: #df3d81;
 }
@@ -263,10 +257,12 @@ setInterval(collectPubkeys, 60000);
   box-sizing: border-box;
   padding: 10px;
 }
+
 .c-feed-profile {
   display: flex;
   gap: 10px;
 }
+
 .c-feed-profile__avatar {
   padding: 0;
   margin: 0;
@@ -274,13 +270,16 @@ setInterval(collectPubkeys, 60000);
   flex-shrink: 0;
   max-width: 3rem;
 }
+
 .c-feed-profile__detail {
   flex-grow: 1;
 }
+
 .c-feed-profile__display-name {
   display: block;
   color: #213547;
 }
+
 .c-feed-profile__user-name {
   display: block;
   font-size: 14px;
@@ -295,6 +294,7 @@ setInterval(collectPubkeys, 60000);
   margin: 0;
   color: #213547;
 }
+
 .c-feed-date {
   font-size: 14px;
   white-space: pre-wrap;
