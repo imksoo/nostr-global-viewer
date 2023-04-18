09640078f97113f39a3147d289fffa6593f26197
kirino.minato@gmail.com <kirino.minato@gmail.com>
2023-04-18 15:48:05 +0900

    Add utter language selection.

--- src/App.vue
+++ src/App.vue
@@ -101,27 +101,43 @@ setInterval(collectProfiles, 1000);
 const synth = window.speechSynthesis;
 async function speakNote(event: nostr.Event) {
   setTimeout(() => {
-    const text = event.content;
+    const display_name = profiles.value.get(event.pubkey)?.display_name + "さん" ?? profiles.value.get(event.pubkey)?.name + '-san';
 
-    const display_name = profiles.value.get(event.pubkey)?.display_name ?? "";
-    const username = display_name ? display_name + "さん " : " ";
-
-    let utterText = username + text;
-    utterText = utterText.replace(
+    let utterUserNameText = display_name;
+    utterUserNameText = utterUserNameText.replace(
       /https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g,
       ""
-    );
-    utterText = utterText.replace(
+    ).replace(
       /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g,
       ""
     );
 
-    const utter = new SpeechSynthesisUtterance(utterText);
-    if (utterText.match(/[亜-熙ぁ-んァ-ヶ]/)) {
-      utter.lang = "ja-JP";
+    const utterUserName = new SpeechSynthesisUtterance(utterUserNameText)
+    if (utterUserNameText.match(/[亜-熙ぁ-んァ-ヶ]/)) {
+      utterUserName.lang = 'ja-JP'
+    } else {
+      utterUserName.lang = 'en-US'
     }
-    utter.volume = volume;
-    synth.speak(utter);
+    utterUserName.volume = volume;
+    synth.speak(utterUserName)
+
+    let utterEventContent = event.content;
+    utterEventContent = utterEventContent.replace(
+      /https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g,
+      ""
+    ).replace(
+      /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g,
+      ""
+    );
+
+    const utterContent = new SpeechSynthesisUtterance(utterEventContent)
+    if (utterEventContent.match(/[亜-熙ぁ-んァ-ヶ]/)) {
+      utterContent.lang = 'ja-JP'
+    } else {
+      utterContent.lang = 'en-US'
+    }
+    utterContent.volume = volume;
+    synth.speak(utterContent)
   }, 1500);
 }
 
@@ -157,6 +173,9 @@ async function login() {
 
 let note = ""
 async function post() {
+  if ( !note ) {
+    return
+  }
   let event = {
     kind: 1,
     tags: [],
