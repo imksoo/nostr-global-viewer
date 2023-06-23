c8d00abd0cfa26aafc088618f69a4426aef0cbe1
Kirino Minato kirino.minato@gmail.com <kirino.minato@gmail.com>
2023-06-24 08:31:14 +0900

    Add OGP support

--- src/components/FeedContent.vue
+++ src/components/FeedContent.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import { onBeforeUnmount, onMounted, ref } from 'vue';
+import { Ref, onBeforeUnmount, onMounted, ref } from 'vue';
 import * as Nostr from "nostr-tools";
+import axios from 'axios';
+import parser from 'html-dom-parser';
 
 import FeedProfile from "./FeedProfile.vue";
 import FeedFooter from "./FeedFooter.vue";
@@ -95,7 +97,7 @@ for (let i = 0; i < props.event.tags.length; ++i) {
 const regex = /(:\w+:|https?:\/\/\S+|(nostr:|@)?(nprofile|nrelay|nevent|naddr|nsec|npub|note)1[023456789acdefghjklmnpqrstuvwxyz]{6,})/;
 
 let rest = props.event.content;
-let tokens: { type: string; content?: any; src?: any; href?: any; id?: string; picture?: any; }[] = [];
+let tokens = ref<{ type: string; content?: any; src?: any; href?: any; id?: string; picture?: any; ogp?: any; }[]>([]);
 
 if (props.event.kind === 6) {
   let note = "";
@@ -108,45 +110,85 @@ if (props.event.kind === 6) {
   rest = `📬りぽすと！ ${note}`;
 }
 
+async function getOgp(url: string, ogp: Ref<{}>) {
+  const res = await axios.get(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`);
+  const dom = parser(res.data.contents);
+  const ogTitleMetaTag = findMetaTag(dom, 'og:title');
+  const ogTitle = (ogTitleMetaTag && ogTitleMetaTag.type === 'tag') ? ogTitleMetaTag.attribs.content : '';
+  const ogImageMetaTag = findMetaTag(dom, 'og:image');
+  const ogImage = (ogImageMetaTag && ogImageMetaTag.type === 'tag') ? ogImageMetaTag.attribs.content : '';
+  const ogDescriptionMetaTag = findMetaTag(dom, 'og:description');
+  const ogDescription = (ogDescriptionMetaTag && ogDescriptionMetaTag.type === 'tag') ? ogDescriptionMetaTag.attribs.content : '';
+
+  if (ogTitle) {
+    ogp.value = {
+      title: ogTitle,
+      image: ogImage,
+      description: ogDescription,
+    };
+  }
+
+  // 再帰的にノードを検索する関数
+  function findMetaTag(nodes: any[], property: string): any {
+    for (const node of nodes) {
+      if (node.type === 'tag' && node.attribs && node.attribs.property === property) {
+        return node;
+      }
+      const found = findMetaTag(node.children || [], property);
+      if (found) {
+        return found;
+      }
+    }
+    return null;
+  }
+}
+
 while (rest.length > 0) {
   const match = rest.match(regex);
   if (match) {
     if (match.index > 0) {
       const text = rest.substring(0, match.index);
-      tokens.push({ type: "text", content: text });
+      tokens.value.push({ type: "text", content: text });
       rest = rest.substring(match.index);
     } else {
       const text = match[0];
       if (text.startsWith(':') && text.endsWith(':')) {
         const emojiName = text.slice(1, -1);
         if (emojiMap.has(emojiName)) {
-          tokens.push({ type: "emoji", content: emojiName, src: emojiMap.get(emojiName) });
+          tokens.value.push({ type: "emoji", content: emojiName, src: emojiMap.get(emojiName) });
         } else {
-          tokens.push({ type: "text", content: text });
+          tokens.value.push({ type: "text", content: text });
         }
       } else if (text.startsWith('http')) {
         try {
           const url = new URL(text);
           const ext = url.pathname.split(".").pop()?.toLocaleLowerCase() ?? "";
           if (['jpg', 'jpeg', 'png', 'gif', 'svg', 'ico', 'bmp', 'webp'].includes(ext)) {
-            tokens.push({ type: "img", src: text });
+            tokens.value.push({ type: "img", src: text });
           } else {
             if (url.hostname === "youtu.be") {
-              tokens.push({ type: "youtube", href: url.pathname, content: decodeURI(text) });
+              tokens.value.push({ type: "youtube", href: url.pathname, content: decodeURI(text) });
             } else if (url.hostname === "www.youtube.com" || url.hostname === "m.youtube.com") {
               if (url.pathname.startsWith("/shorts/")) {
                 const v = url.pathname.replace('/shorts/', '');
-                tokens.push({ type: "youtube", href: v, content: decodeURI(text) });
+                tokens.value.push({ type: "youtube", href: v, content: decodeURI(text) });
               } else {
                 const v = getParam('v', text);
-                tokens.push({ type: "youtube", href: v, content: decodeURI(text) });
+                tokens.value.push({ type: "youtube", href: v, content: decodeURI(text) });
               }
             } else if (url.hostname.endsWith("twitter.com")) {
               const src = encodeURI(text);
               twitterSources.push(src);
-              tokens.push({ type: "twitter", href: text, src, content: decodeURI(text) });
+              tokens.value.push({ type: "twitter", href: text, src, content: decodeURI(text) });
             } else {
-              tokens.push({ type: "link", href: text, content: decodeURI(text) });
+              try {
+                const ogp = ref({});
+                getOgp(text, ogp);
+                tokens.value.push({ type: "link", href: text, content: decodeURI(text), ogp });
+              } catch (error) {
+                console.log(error);
+                tokens.value.push({ type: "link", href: text, content: decodeURI(text) });
+              }
             }
 
             function getParam(name: string, url: string): string | null {
@@ -160,7 +202,7 @@ while (rest.length > 0) {
             }
           }
         } catch (err) {
-          tokens.push({ type: "text", content: text });
+          tokens.value.push({ type: "text", content: text });
         }
       } else {
         try {
@@ -169,21 +211,21 @@ while (rest.length > 0) {
             case "nevent": {
               const href = 'https://nostx.shino3.net/' + Nostr.nip19.noteEncode(data.data.id);
               const id = data.data.id;
-              tokens.push({ type: 'nostr-note', content: text, href, id });
+              tokens.value.push({ type: 'nostr-note', content: text, href, id });
             } break;
             case "note": {
               const href = 'https://nostx.shino3.net/' + Nostr.nip19.noteEncode(data.data);
               const id = data.data;
-              tokens.push({ type: 'nostr-note', content: text, href, id });
+              tokens.value.push({ type: 'nostr-note', content: text, href, id });
             } break;
             case "nprofile": {
               const href = 'https://nostx.shino3.net/' + Nostr.nip19.npubEncode(data.data.pubkey);
               if (props.getProfile) {
                 const profile = props.getProfile(data.data.pubkey);
                 const name = profile.display_name || profile.name || profile.pubkey.substring(profile.pubkey.length - 8)
-                tokens.push({ type: 'nostr-npub', content: "@" + name, href, picture: profile.picture });
+                tokens.value.push({ type: 'nostr-npub', content: "@" + name, href, picture: profile.picture });
               } else {
-                tokens.push({ type: 'nostr', content: text, href });
+                tokens.value.push({ type: 'nostr', content: text, href });
               }
             } break;
             case "npub": {
@@ -191,25 +233,25 @@ while (rest.length > 0) {
               if (props.getProfile) {
                 const profile = props.getProfile(data.data);
                 const name = profile.display_name || profile.name || profile.pubkey.substring(profile.pubkey.length - 8)
-                tokens.push({ type: 'nostr-npub', content: "@" + name, href, picture: profile.picture })
+                tokens.value.push({ type: 'nostr-npub', content: "@" + name, href, picture: profile.picture })
               } else {
-                tokens.push({ type: 'nostr', content: text, href });
+                tokens.value.push({ type: 'nostr', content: text, href });
               }
             } break;
             default: {
               const href = text;
-              tokens.push({ type: 'nostr', content: text, href });
+              tokens.value.push({ type: 'nostr', content: text, href });
             }
           }
         } catch (err) {
-          tokens.push({ type: "text", content: text });
+          tokens.value.push({ type: "text", content: text });
         }
       }
       rest = rest.substring(text.length);
     }
   } else {
     const text = rest;
-    tokens.push({ type: "text", content: text });
+    tokens.value.push({ type: "text", content: text });
     rest = "";
   }
 }
@@ -230,8 +272,15 @@ while (rest.length > 0) {
         <span v-else>{{ token.content }}</span>
       </template>
       <template v-else-if="token?.type === 'link'">
+        <a :href="token.href" target="_blank" referrerpolicy="no-referrer">{{ token.content }}</a>
         <a :href="token.href" target="_blank" referrerpolicy="no-referrer">
-          {{ token.content }}
+          <div class="c-feed-content-ogp" v-if="token.ogp.title">
+            <div class="c-feed-content-ogp-title">{{ token.ogp.title }}</div>
+            <div class="c-feed-content-ogp-box">
+              <img :src="token.ogp.image" class="c-feed-content-ogp-image" referrerpolicy="no-referrer" />
+              <span class="c-feed-content-ogp-description">{{ token.ogp.description }}</span>
+            </div>
+          </div>
         </a>
       </template>
       <template v-else-if="token?.type === 'youtube'">
@@ -315,6 +364,34 @@ while (rest.length > 0) {
   font-size: 0.8em;
   margin: 0.2em 1.5em;
   padding: 0.5em;
+}
+
+.c-feed-content-ogp {
+  border: gray groove 1px;
+  font-size: 0.8em;
+  margin: 0.2em 1.5em;
+  padding: 0.5em;
+  color: #213547;
+}
+
+.c-feed-content-ogp-box {
+  display: flex;
+}
+
+.c-feed-content-ogp-title {
+  font-size: 1.2em;
+  flex-basis: 100%;
+  text-decoration: underline;
+}
+
+.c-feed-content-ogp-image {
+  max-width: 160px;
+  max-height: 480px;
+  margin: auto 8px;
+}
+
+.c-feed-content-ogp-description {
+  font-size: 0.8em;
 }
 
 .c-feed-content-image {
