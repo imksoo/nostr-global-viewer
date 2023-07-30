081b81dcdb204636057cd6d5a0d25512a80c6e9e
Kirino Minato kirino.minato@gmail.com <kirino.minato@gmail.com>
2023-07-30 22:59:21 +0900

    Revise default relay

--- src/views/top.vue
+++ src/views/top.vue
@@ -32,12 +32,12 @@ let profileRelays = [
   "wss://nostr-pub.wellorder.net/",
   "wss://nostr-relay.nokotaro.com/",
   "wss://nostr.fediverse.jp",
-  "wss://nostr.h3z.jp/",
   "wss://nostr.holybea.com/",
   "wss://nrelay-jp.c-stellar.net",
   "wss://nrelay.c-stellar.net",
   "wss://offchain.pub/",
   "wss://r.kojira.io/",
+  "wss://rbr.bio/",
   "wss://relay-jp.nostr.wirednet.jp/",
   "wss://relay.austrich.net/",
   "wss://relay.current.fyi/",
@@ -46,10 +46,6 @@ let profileRelays = [
   "wss://relay.nostr.wirednet.jp/",
   "wss://relay.snort.social/",
   "wss://yabu.me/",
-];
-
-const invalidSinceLastRelays = [
-  "wss://relay.nostr.band/",
 ];
 
 const pool = new RelayPool(normalizeUrls(feedRelays), {
