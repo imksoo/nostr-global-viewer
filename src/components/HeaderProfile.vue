1f5bb09af5de7bddf0e258fae04a88d4e95c485a
Kirino Minato kirino.minato@gmail.com <kirino.minato@gmail.com>
2023-08-11 08:51:29 +0900

    Background transparency

--- src/components/HeaderProfile.vue
+++ src/components/HeaderProfile.vue
@@ -19,13 +19,14 @@ const backgroundImage = ref("url(" + props.profile.banner + ")");
 
 <template>
   <div class="p-header-profile" :style="{ backgroundImage: backgroundImage }">
+    <div class="p-header-profile-content">
     {{ JSON.stringify(props.profile, undefined, 1) }}
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .p-header-profile {
-  padding: 12px;
   color: white;
   text-shadow: 2px 2px 8px gray,
     -2px 2px 8px gray,
@@ -34,5 +35,10 @@ const backgroundImage = ref("url(" + props.profile.banner + ")");
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+
+  &-content {
+    padding: 12px;
+    background: rgba(0, 0, 0, 0.6);
+  }
 }
 </style>
