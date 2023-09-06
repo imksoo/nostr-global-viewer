50b5797a95de8a1fa131112a7d2484b084dadc86
Kirino Minato kirino.minato@gmail.com <kirino.minato@gmail.com>
2023-09-06 15:08:20 +0900

    Add NosTx button for profile.

--- src/components/FeedProfile.vue
+++ src/components/FeedProfile.vue
@@ -1,43 +1,44 @@
 <script setup lang="ts">
-import * as nostr from "nostr-tools"
+import * as nostr from "nostr-tools";
 
 const props = defineProps({
   profile: {
     type: Object as () => {
-      pubkey: string
-      picture: string | undefined
-      display_name: string | undefined
-      name: string | undefined
+      pubkey: string;
+      picture: string | undefined;
+      display_name: string | undefined;
+      name: string | undefined;
     },
     required: true,
   },
-})
+});
 
 function getProfileLink(pubkey: string) {
   if (pubkey) {
-    return "?" + nostr.nip19.npubEncode(pubkey)
+    return "?" + nostr.nip19.npubEncode(pubkey);
   } else {
-    return ""
+    return "";
   }
 }
 
 function copyNpubId(): void {
-  const text = nostr.nip19.npubEncode(props.profile.pubkey)
-  copyToClipboard("nostr:" + text)
+  const text = nostr.nip19.npubEncode(props.profile.pubkey);
+  copyToClipboard("nostr:" + text);
 }
 
 async function copyToClipboard(text: string) {
   try {
-    await navigator.clipboard.writeText(text)
+    await navigator.clipboard.writeText(text);
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 
 function onImageError(e: Event) {
-  const placehold = "https://placehold.jp/391e6c/d7c6c6/60x60.png?text=Image%0ANot%20Found"
-  const target = e.target as HTMLImageElement
-  target.src = placehold
+  const placehold =
+    "https://placehold.jp/391e6c/d7c6c6/60x60.png?text=Image%0ANot%20Found";
+  const target = e.target as HTMLImageElement;
+  target.src = placehold;
 }
 </script>
 <template>
@@ -55,7 +56,10 @@ function onImageError(e: Event) {
     <div class="c-feed-profile-parts">
       <div class="c-feed-profile-display-name">
         <p>
-          <span class="c-feed-profile-copy-button" @click="(_$event) => { copyNpubId() }">
+          <span @click="(_$event) => {
+            copyNpubId();
+          }
+            ">
             <mdicon name="content-copy" :width="14" :height="14" title="Copy npub string" />
           </span>
           <span>&thinsp;</span>{{
@@ -66,7 +70,15 @@ function onImageError(e: Event) {
         </p>
       </div>
       <div class="c-feed-profile-user-name">
-        <a target="_blank" :href="getProfileLink(props.profile.pubkey)">@{{ props.profile.name ?? "" }}</a>
+        <a target="_blank" :href="'https://nostx.shino3.net/' +
+            nostr.nip19.npubEncode(props.profile.pubkey)
+            ">
+          <mdicon name="open-in-new" :width="14" :height="14" title="Open NosTx" />
+        </a>
+        <span>&thinsp;</span>
+        <a target="_blank" :href="getProfileLink(props.profile.pubkey)">{{
+          props.profile.name ? "@" + props.profile.name : ""
+        }}</a>
       </div>
     </div>
   </div>
@@ -121,12 +133,6 @@ function onImageError(e: Event) {
     -webkit-box-orient: vertical;
     -webkit-line-clamp: 1;
     overflow: hidden;
-  }
-
-  &-copy-button {
-    display: inline;
-    color: #213547;
-    vertical-align: bottom;
   }
 }
 </style>
