5c9d3ebb3884325a3ca94930dcb3b849cea4697c
Kirino Minato kirino.minato@gmail.com <kirino.minato@gmail.com>
2023-09-20 16:56:23 +0900

    .

--- src/components/FeedContent.vue
+++ src/components/FeedContent.vue
@@ -203,8 +203,8 @@ while (rest.length > 0) {
                 const v = getParam('v', text);
                 tokens.value.push({ type: "youtube", href: v, content: decodeURI(text) });
               }
-            } else if (url.hostname.endsWith("twitter.com")) {
-              const src = encodeURI(text);
+            } else if (url.hostname.endsWith("twitter.com") || url.hostname.endsWith("x.com")) {
+              const src = encodeURI(text.replace("x.com", "twitter.com"));
               twitterSources.push(src);
               tokens.value.push({ type: "twitter", href: text, src, content: decodeURI(text) });
             } else {
