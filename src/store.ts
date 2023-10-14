c6ca3245bd78688d649065a5813412fe4bd09ac2
Kirino Minato <kirino.minato@gmail.com>
2023-10-14 13:21:20 +0000

    Remove current.fyi

--- src/store.ts
+++ src/store.ts
@@ -18,7 +18,6 @@ export const profileRelays = [
   "wss://r.kojira.io/",
   "wss://relay-jp.nostr.wirednet.jp/",
   "wss://relay-jp.shino3.net/",
-  "wss://relay.current.fyi/",
   "wss://relay.damus.io/",
   "wss://relay.nostr.band/",
   "wss://relay.nostr.wirednet.jp/",
