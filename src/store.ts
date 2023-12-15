fbc4a4bf1f9f52191ede1c78eafcca4ab566b385
Kirino Minato <kirino.minato@gmail.com>
2023-12-15 07:02:57 +0000

    Refactor url code

--- src/store.ts
+++ src/store.ts
@@ -34,25 +34,39 @@ export const pool = new RelayPool(normalizeUrls(feedRelays), {
   useEventCache: true,
   skipVerification: false,
 });
-pool.onerror((url, msg) => { console.log("pool.error", url, msg) });
-pool.onnotice((url, msg) => { console.log("pool.onnotice", url, msg) });
-pool.ondisconnect((url, msg) => { console.log("pool.ondisconnect", url, msg) });
+pool.onerror((url, msg) => {
+  console.log("pool.error", url, msg);
+});
+pool.onnotice((url, msg) => {
+  console.log("pool.onnotice", url, msg);
+});
+pool.ondisconnect((url, msg) => {
+  console.log("pool.ondisconnect", url, msg);
+});
 
 export function normalizeUrls(urls: string[]): string[] {
-  return urls.map((url) => (Nostr.utils.normalizeURL(url)));
+  return urls.map((url) => {
+    let u = url;
+    
+    if (u.startsWith("http://")) { u = u.replace("http://", "ws://"); }
+    else if (u.startsWith("https://")) { u = u.replace("https://", "wss://"); }
+    else if (!(u.startsWith("ws://") || u.startsWith("wss://"))) { u = "wss://" + u; }
+
+    return Nostr.utils.normalizeURL(u);
+  });
 }
 
 export interface NostrEvent {
-  id: string,
-  sig: string,
-  pubkey: string,
-  kind: Nostr.Kind | number,
-  content: string,
-  tags: string[][],
-  created_at: number,
-  isReposted: Boolean | undefined,
-  isFavorited: Boolean | undefined,
-};
+  id: string;
+  sig: string;
+  pubkey: string;
+  kind: Nostr.Kind | number;
+  content: string;
+  tags: string[][];
+  created_at: number;
+  isReposted: Boolean | undefined;
+  isFavorited: Boolean | undefined;
+}
 
 export const events = ref(new Array<NostrEvent>());
 export const eventsToSearch = ref(new Array<NostrEvent>());
