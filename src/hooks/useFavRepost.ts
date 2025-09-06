79139a01646daf4238f0dfb3e27fc1344d308049
Kirino Minato <kirino.minato@gmail.com>
2025-09-06 01:47:38 +0000

    Add client tag to reaction events in useFavRepost hook

--- src/hooks/useFavRepost.ts
+++ src/hooks/useFavRepost.ts
@@ -1,4 +1,5 @@
 import * as Nostr from 'nostr-tools';
+import { feedRelays } from "../store";
 
 type NostrEvent = {
   id: string,
@@ -16,12 +17,14 @@ export function createFavEvent(reacted: NostrEvent) {
     (tag) => tag.length >= 2 && (tag[0] === 'e' || tag[0] === 'p'),
   )
 
+
   const reaction = Nostr.getBlankEvent(7);
   reaction.tags = [
     ...inheritedTags,
     ['e', reacted.id],
     ['p', reacted.pubkey],
     ['k', reacted.kind.toString()],
+    ["client", "Nozokimado", "31990:ad73ce27d83ccc6bf6184549e529119d8b5963c5e6f681f6690a33f91c8b615a:1757113040", feedRelays[0] || ""],
   ]
   reaction.content = "🪟";
   reaction.created_at = Math.floor(Date.now() / 1000);
@@ -34,6 +37,7 @@ export function createRepostEvent(reposted: NostrEvent) {
   reaction.tags = [
     ['e', reposted.id],
     ['p', reposted.pubkey],
+    ["client", "Nozokimado", "31990:ad73ce27d83ccc6bf6184549e529119d8b5963c5e6f681f6690a33f91c8b615a:1757113040", feedRelays[0] || ""],
   ]
   reaction.content = "";
   reaction.created_at = Math.floor(Date.now() / 1000);
